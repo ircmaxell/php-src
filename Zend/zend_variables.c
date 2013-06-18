45eb2290b2f3c9928405e3e321da6c1d6fd9fc9c
Anthony Ferrara <ircmaxell@gmail.com>
2013-06-18 12:35:36 -0400

    Reduce GC lockout to Object deref only

--- Zend/zend_variables.c
+++ Zend/zend_variables.c
@@ -29,7 +29,6 @@
 
 ZEND_API void _zval_dtor_func(zval *zvalue ZEND_FILE_LINE_DC)
 {
-	LOCK_GC()
 	switch (Z_TYPE_P(zvalue) & IS_CONSTANT_TYPE_MASK) {
 		case IS_STRING:
 		case IS_CONSTANT:
@@ -51,8 +50,10 @@ ZEND_API void _zval_dtor_func(zval *zvalue ZEND_FILE_LINE_DC)
 		case IS_OBJECT:
 			{
 				TSRMLS_FETCH();
-
+				
+				LOCK_GC()
 				Z_OBJ_HT_P(zvalue)->del_ref(zvalue TSRMLS_CC);
+				UNLOCK_GC()
 			}
 			break;
 		case IS_RESOURCE:
@@ -70,7 +71,6 @@ ZEND_API void _zval_dtor_func(zval *zvalue ZEND_FILE_LINE_DC)
 		default:
 			break;
 	}
-	UNLOCK_GC()
 }
 
 
