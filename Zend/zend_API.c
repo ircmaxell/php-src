5e71fb581f82cb3fd19da70731d6617113f08ea0
Anthony Ferrara <ircmaxell@gmail.com>
2013-08-30 13:27:20 -0400

    Refactor out the lookup in is_callable

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -2764,7 +2764,7 @@ static int zend_is_callable_check_func(int check_flags, zval *callable, zend_fca
 		}
 		/* Check if function with given name exists.
 		 * This may be a compound name that includes namespace name */
-		if (zend_lookup_function(lmname, mlen, (void**)&fcc->function_handler) == SUCCESS) {
+		if (ZEND_LOOKUP_FUNCTION_BY_NAME(lmname, mlen, &fcc->function_handler)) {
 			efree(lmname);
 			return 1;
 		}
