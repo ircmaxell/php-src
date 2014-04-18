9e48e181ce3b2cd16417811018ec2aec767a0e19
Anthony Ferrara <ircmaxell@gmail.com>
2014-04-18 16:05:29 -0400

    Add a few more tests

--- Zend/zend_operators_test.cpp
+++ Zend/zend_operators_test.cpp
@@ -3,4 +3,15 @@
 
 TEST(zend_atoi, basicOperation) {
 	EXPECT_EQ(1024, zend_atoi("1k", 2));
+	EXPECT_EQ(2048, zend_atoi("2k", 2));
+	EXPECT_EQ(3145728, zend_atoi("3m", 2));
+	// check for 32 bit overflow
+        EXPECT_EQ(0, zend_atoi("4g", 2));
+	EXPECT_EQ(1, zend_atoi("1", 1));
+}
+
+TEST(zend_string_to_double, basicOperation) {
+	EXPECT_EQ(1.0, zend_string_to_double("1", 1));
+	EXPECT_EQ(1.5, zend_string_to_double("1.5", 3));
+	EXPECT_EQ(1.5, zend_string_to_double("1.5fed", 6));
 }
